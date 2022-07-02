db65e338ae89cc03d8bc66f46c27ac4168d90af8
Akriti Jaiswal <akritij121btechcse2025@kccitm.edu.in>
2022-07-02 10:47:26 +0530

    to check even or odd

---
+++ evenOdd.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+
+    printf("Enter the Number --> ");
+    scanf("%d", &num);
+
+    if(num%2 == 0)
+    printf("Even");
+    
+    else
+    printf("Odd");
+
+    return 0;
+}
